13ddc28c8fafb401e426684bf80c5837bd848ecc
Mirkof13 <miguel13.mirkof@gmail.com>
2026-09-08 19:41:51 -0400

    fix(test): actualizar asercion de titulo en app.spec.ts para pruebas unitarias

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -19,6 +19,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hackathon: transformaciones con matrices');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Modelo 3D y Transformaciones Matriciales Afines');
   });
 });
